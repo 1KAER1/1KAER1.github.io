021995f2c06c33717392daacef75b419eb8fd757
Kacper Rolewski <kacper.r17@gmail.com>
2021-10-24 10:36:47 +0200

    First version of white strap going down the road

--- Lab3.js
+++ Lab3.js
@@ -29,23 +29,35 @@ class Actor {
     }
 
     update() {
-        this.drawArc();
-        this.y = this.y - this.velocity;
+        this.y = this.y + this.velocity;
     }
 }
 
-
+//Road line properties
+var roadLineWidth = 10;
+var roadLineHeight = 75;
 
 // Road
 var road = new Actor(100, 0, canvas.width - 200, canvas.height, "#A8A8A8", 0);
 
+var roadLines = [];
 
+function drawRoadLines() {
+    roadLines.push(new Actor(canvas.width / 2 - roadLineWidth / 2, 0, roadLineWidth, roadLineHeight, "#FFFFFF", 1));
+}
 
-function animate()
-{
+function animate() {
     window.requestAnimationFrame(animate);
     road.drawRectangle();
+
+    roadLines.forEach(function (roadLine) {
+            roadLine.drawRectangle();
+            roadLine.update();
+    });
+
+
 
 }
 
 animate();
+setInterval(drawRoadLines, 4);
