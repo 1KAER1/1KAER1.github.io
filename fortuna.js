8d1f8f0fa78065ade36373067203a0b236b45f75
Kacper Rolewski <kacper.r17@gmail.com>
2021-11-13 18:12:18 +0100

    Created wheel of fortune for WWW

--- fortuna.js
+++ fortuna.js
@@ -1,49 +1,99 @@
-
 var game = {
-  zdobyte: 0,
-  zycia: 1,
+  score: 0,
+  lives: 6,
 }
 
-//alert(data[0]['country']);
-//var elem = document.getElementById("panstwa");
-//elem.innerHTML = data[0]['country'];
+var lost = false;
+var checkedLetters = [];
 
-//alert(data.length);
-//alert(data[0]['country'][2]);
+var countryToGuess = data[getRandomInt(0, data.length)]['country'];
+spawnHiddenWord(countryToGuess);
 
-for (var i = 0; i < data[0]["country"].length; i += 1) {
-  //alert(data[0]['country'][i]);
+document.getElementById("score").innerHTML = game.score;
+document.getElementById("guessCount").innerHTML = game.lives;
+
+console.log(countryToGuess);
+
+function spawnHiddenWord(countryToGuess) {
+  var playerGuess = document.getElementById('playerGuess');
+  var block = createNewBlock();
+  for (var i = 0; i < countryToGuess.length; i++) {
+    if (countryToGuess[i] === ' ') {
+      playerGuess.appendChild(block);
+      block = createNewBlock();
+    } else {
+      block.appendChild(insertEmptyBlock(i));
+    }
+    playerGuess.append(block);
+  }
 }
 
 
 
-addElement("wrap");
-//LISTENERS
-
-document.getElementById("graj").addEventListener("click", Sprawdz_Litery);
-//alert(game.zycia);
-
-
-//FUNKCJE
-function Sprawdz_Litery() {
-  var liter = document.getElementById("wpisz_litere").value;
-  alert(liter);
-  alert(getRandomInt(10, 20));
+function createNewBlock() {
+  var block = document.createElement('div');
+  block.classList.add('block');
+  return block;
 }
 
+function insertEmptyBlock(i) {
+  var emptyBlock = document.createElement('div');
+  emptyBlock.i = 'char-' + i;
+  emptyBlock.classList.add('emptyBlock');
+  emptyBlock.setAttribute("id", 'block' + i);
+  emptyBlock.innerHTML = countryToGuess[i];
+  return emptyBlock;
+}
 
-function addElement(mydiv) {
+document.getElementById("graj").addEventListener("click", checkLetters);
 
-  my_div = document.getElementById(mydiv);
+function checkLetters() {
+  var letter = document.getElementById('wpiszLitere').value;
+  var occurrences = 0;
 
-  for (var i = 0; i <= data[66]["country"].length - 1; i++) {
-    newDiv2 = document.createElement("span");
-    newDiv2.classList.add("letter")
-    newDiv2.innerHTML = data[66]["country"][i];
-    document.body.appendChild(newDiv2)
+  if (game.lives == 0) {
+    alert("Przegrałeś!");
+    lost = true;
+    return;
   }
 
-  
+  if(game.score == countryToGuess.length)
+  {
+    alert("Wygrałeś!");
+    return;
+  }
+
+  if (checkedLetters.includes(letter)) {
+    alert("Sprawdzano już tą literę!");
+    return;
+  } else {
+    checkedLetters.push(letter);
+  }
+
+  for (var i = 0; i < countryToGuess.length; i++) {
+    if (letter.toLowerCase() === countryToGuess[i].toLowerCase()) {
+      document.getElementById('block' + i).style.color = 'rgb(255, 255, 255)';
+      occurrences++;
+    }
+  }
+
+  if (occurrences > 0) {
+    game.score += occurrences;
+    document.getElementById("score").innerHTML = game.score;
+  } else {
+    game.lives--;
+    document.getElementById("guessCount").innerHTML = game.lives;
+    if (game.lives == 0) {
+      alert("Przegrałeś!");
+    }
+  }  
+
+  if(game.score == countryToGuess.length)
+  {
+    alert("Wygrałeś!");
+    return;
+  }
+
 }
 
 function getRandomInt(min, max) {
