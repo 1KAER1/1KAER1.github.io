f8509990b04ee4168c65ff3b9bf1f3ff2be48c10
Kacper Rolewski <kacper.r17@gmail.com>
2021-11-28 09:57:27 +0100

    Checking if PESEL is valid

---
+++ pesel.js
@@ -0,0 +1,42 @@
+function validateOnePesel(){
+    var worker = new Worker('validate_one_pesel.js');
+  
+    worker.addEventListener('message', function(e) {
+      let pesels = document.getElementById("pesels");
+      pesels.innerHTML = e.data;
+    }, false);
+  
+    var year = document.getElementById("year").value;
+    var month = document.getElementById("month").value;
+    var day = document.getElementById("day").value;
+    var pesel = document.getElementById("numbers").value;
+    worker.postMessage([year,month,day,pesel]);
+  }
+  
+  function showAllPesels(){
+    var worker1 = new Worker('show_all_pesels.js');
+  
+    worker1.addEventListener('message', function(e) {
+      let pesels = document.getElementById("pesels");
+      pesels.innerHTML = e.data.join("\n");
+    }, false);
+  
+    var year = document.getElementById("year").value;
+    var month = document.getElementById("month").value;
+    var day = document.getElementById("day").value;
+  
+    worker1.postMessage([year,month,day]);
+  }
+  
+  function byNumber(){
+    var worker2 = new Worker('find_valid_pesels.js');
+  
+    worker2.addEventListener('message', function(e) {
+      let pesels = document.getElementById("pesels");
+      pesels.innerHTML = e.data.join("\n");
+    }, false);
+  
+    var pesel = document.getElementById("numbers").value;
+  
+    worker2.postMessage(pesel);
+  }
---
+++ validate_one_pesel.js
@@ -0,0 +1,29 @@
+this.addEventListener('message', function (e) {
+  var data = e.data;
+  var PESEL = "";
+  PESEL = data[0].substr(2, 3) + data[1] + data[2] + data[3];
+  this.postMessage(validate(PESEL));
+}, false);
+
+function validate(pesel) {
+  var weight = new Array(1, 3, 7, 9, 1, 3, 7, 9, 1, 3);
+  var sum = 0;
+  var ok = true;
+
+  ok = ok && (pesel.substr(4, 2) >= "01" && pesel.substr(4, 2) <= "31");
+
+  for (var i = 0; i < weight.length; i++) {
+    sum += (parseInt(pesel.substring(i, 1)) * weight[i]);
+  }
+
+  var check = 10 - (sum % 10);
+  if (check == 10) {
+    check = 0;
+  }
+
+  if (check == pesel.substr(pesel.length - 1, 1) && ok) {
+    return "To jest prawidłowy PESEL";
+  } else {
+    return "To nie jest prawidłowy PESEL";
+  }
+}
