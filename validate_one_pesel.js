25ff9cfa3091811de743033414adef0427c18a81
Kacper Rolewski <kacper.r17@gmail.com>
2021-11-28 10:02:49 +0100

    Checking if PESEL is valid 3

--- validate_one_pesel.js
+++ validate_one_pesel.js
@@ -1,6 +1,10 @@
 this.addEventListener('message', function (e) {
   var data = e.data;
   var PESEL = "";
+  console.log(data[0]);
+  console.log(data[1]);
+  console.log(data[2]);
+  console.log(data[3]);
   PESEL = data[0] + data[1] + data[2] + data[3];
   this.postMessage(validate(PESEL));
 }, false);
